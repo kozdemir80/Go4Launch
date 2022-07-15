6629c54a17d4bfcba16c3ffb1e1cfd0eec01595d
kozdemir80 <k.ozdemir80@gmail.com>
2022-07-15 05:12:28 +0300

    Mentor review

---
+++ app/src/main/java/com/example/go4launch/model/userdetails/NotificationData.kt
@@ -0,0 +1,7 @@
+package com.example.go4launch.model.userdetails
+
+data class NotificationData(
+    val title:String?=null,
+    val message:String?=null
+
+)
---
+++ app/src/main/java/com/example/go4launch/model/userdetails/PushNotification.kt
@@ -0,0 +1,6 @@
+package com.example.go4launch.model.userdetails
+
+data class PushNotification(
+    val data: NotificationData,
+    val to:String
+)
